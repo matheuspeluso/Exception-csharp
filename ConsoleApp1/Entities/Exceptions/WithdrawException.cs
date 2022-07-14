903824f01d9c3003b34527c15832eaccd3f21566
Matheus Peluso <matheuspeluso17@gmail.com>
2022-07-14 19:11:59 -0300

    tratamento de exception com c#

---
+++ ConsoleApp1/Entities/Exceptions/WithdrawException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1.Entities.Exceptions
+{
+    internal class WithdrawException : ApplicationException
+    {
+        public WithdrawException(string message): base(message)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ConsoleApp1.Entities;
+using ConsoleApp1.Entities.Exceptions;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine());
+
+                Account account = new Account(number, holder, initialBalance, withdrawLimit);
+                Console.Write("Enter amount for withdraw: ");
+                double withdrawAmount = double.Parse(Console.ReadLine());
+                account.Withdraw(withdrawAmount);
+
+                Console.WriteLine("New Balance :" + account.Balance);
+            }
+            catch(WithdrawException e)
+            {
+                Console.WriteLine("Withdraw error: "+e.Message);
+            }
+        }
+    }
+}
